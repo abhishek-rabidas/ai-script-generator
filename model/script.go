ca30c9e15a3b4dc5f440fdd54d7a80b1d09a4398
abhishek-rabidas <avisec07@gmail.com>
2024-04-14 16:28:15 +0530

    foriegn key issue fix

--- model/script.go
+++ model/script.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type Script struct {
 	gorm.Model
-	Uid      string `gorm:"not null; unique; size:64"`
-	Account  Account
-	Platform string
-	Duration int
-	Topic    string
-	Content  string `gorm:"size:10240"`
+	Uid       string `gorm:"not null; unique; size:64"`
+	Account   Account
+	AccountId uint
+	Platform  string
+	Duration  int
+	Topic     string
+	Content   string `gorm:"size:10240"`
 }
